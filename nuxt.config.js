a2a9f68a03e482ddda0d6f45055eeddf02c244b2
yamady0704 <fleis_preis@yahoo.co.jp>
2020-01-14 15:08:50 +0900

    create card design

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,13 +45,22 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
   /*
   ** Axios module configuration
   */
   axios: {
-    credentials: true
+    credentials: true,
+    proxy: true
+  },
+
+  proxy: {
+    "/api/": {
+      target: "https://newsapi.org/v2/",
+      pathRewrite: { "^/api/": "" }
+    }
   },
 
   env: {
