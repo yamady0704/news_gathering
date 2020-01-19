dc486c957acd962a55cae5cc2631345e9fc8d33e
yamady0704 <fleis_preis@yahoo.co.jp>
2020-01-20 06:00:28 +0900

    creating headline page

--- pages/index.vue
+++ pages/index.vue
@@ -124,7 +124,7 @@
               <md-button @click="addHeadlineToFeed(headline)" class="md-icon-button" :class="isInFeed(headline.title)">
                 <md-icon>bookmark</md-icon>
               </md-button>
-              <md-button class="md-icon-button">
+              <md-button @click="saveHeadline(headline)" class="md-icon-button">
                 <md-icon>message</md-icon>
               </md-button>
             </md-card-actions>
@@ -197,6 +197,11 @@
       },
       async removeHeadlineFromFeed(headline) {
         await this.$store.dispatch('removeHeadlineFromFeed', headline);
+      },
+      async saveHeadline(headline) {
+        await this.$store.dispatch('saveHeadline', headline).then(() => {
+          this.$router.push(`/headlines/${headline.slug}`);
+        });
       },
       logoutUser() {
         this.$store.dispatch("logoutUser");
